719b7ae0993bd7a25be1fd4e767fc5329966a56f
Hugo <dev.hugoregadas@gmail.com>
2021-12-08 10:26:36 +0100

    yarn/firebase fix

--- .history/src/firebase_20210308133340.js
+++ .history/src/firebase_20210308133340.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCcJArQjZ-hFxpTD7_m6rrhw_v-HrmQyk8",
-  authDomain: "clone-de64b.firebaseapp.com",
-  projectId: "clone-de64b",
-  storageBucket: "clone-de64b.appspot.com",
-  messagingSenderId: "1072854293266",
-  appId: "1:1072854293266:web:cccbc28f907f662b30b1cb"
+  apiKey: "AIzaSyA8uBQeqbv3gJh-kdCoeSnkaPR53FqL6FA",
+  authDomain: "clone-5e617.firebaseapp.com",
+  projectId: "clone-5e617",
+  storageBucket: "clone-5e617.appspot.com",
+  messagingSenderId: "59927452747",
+  appId: "1:59927452747:web:e226ad8b6d0813b01201ce",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
